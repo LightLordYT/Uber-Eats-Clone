eecc2c5e789613a5fa3e6dd38e7d599fc371e079
LightLord <stevensah88@gmail.com>
2022-07-27 08:14:09 -0700

    js now processes array almost perfectly

--- script.js
+++ script.js
@@ -3,35 +3,35 @@ const restaurantData = [
         name: "McDonald's",
         img: "https://cdn.shopify.com/s/files/v2/upload/v5/restaurant_data.png",
         description: "Fast Food, Fast Service",
-        rating: 4.5,
+        rating: '4.5',
     },
     {
         name: "Wendy's",
         img: "",
         description: "Amazing Food",
-        rating: 3.5,
+        rating: '3.5',
     },
     {
         name: "Ichiran Sushi",
         img: "https://cdn.shopify.com/s/files/v2/upload/v5/",
         description: "Best Sushi Ever.",
-        rating: 4.5
+        rating: '4.5'
     },
     {
         name: "Kenta Ramen",
         img: "https://cdn.shopify.com/s/",
         description: "Best Ramen Out There.",
-        rating: 4
+        rating: '4'
     },
     {
         name: "Burger King",
         img: "https://cdn.shopify.com/s/files/v2/upload/",
         description: "King of Burgers.",
-        rating: 4
+        rating: '4'
     }
 ]
 
-const names = restaurantData.map((rest) => {
+restaurantData.forEach((rest) => {
     const body = document.body
     const div = document.createElement("div")
     const span = document.createElement('span')
@@ -42,4 +42,15 @@ const names = restaurantData.map((rest) => {
     span.className = 'desc'
     div.appendChild(span)
     body.append(div)
+    let numbs = rest.rating.split('.')
+    for(i=0;i<numbs[0]; i++){
+        let div3 = document.createElement('i')
+        div3.className = 'bi bi-star-fill'
+       div.append(div3)
+    }
+    if(numbs[1]){
+        let div4 = document.createElement('i')
+        div4.className = 'bi bi-star-half'
+        div.append(div4)
+       }
 })
