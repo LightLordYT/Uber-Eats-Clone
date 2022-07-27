d2775258cde4390b0b51b6bc5b97030b9ba6a5b8
LightLord <stevensah88@gmail.com>
2022-07-27 08:23:42 -0700

    got images to work

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const restaurantData = [
     {
         name: "McDonald's",
-        img: "https://cdn.shopify.com/s/files/v2/upload/v5/restaurant_data.png",
+        img: "https://cdn.cdnlogo.com/logos/m/12/mcdonalds.svg",
         description: "Fast Food, Fast Service",
         rating: '4.5',
     },
     {
         name: "Wendy's",
-        img: "",
+        img: "https://cdn.cdnlogo.com/logos/w/19/wendys.svg",
         description: "Amazing Food",
         rating: '3.5',
     },
     {
         name: "Ichiran Sushi",
-        img: "https://cdn.shopify.com/s/files/v2/upload/v5/",
+        img: "https://cdn.cdnlogo.com/logos/s/10/sushiswap.svg",
         description: "Best Sushi Ever.",
         rating: '4.5'
     },
     {
         name: "Kenta Ramen",
-        img: "https://cdn.shopify.com/s/",
+        img: "https://scontent.ftpe7-4.fna.fbcdn.net/v/t31.18172-8/474630_563104630401057_1048793268_o.png?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qk_p1ksKDmgAX_7Ajum&_nc_ht=scontent.ftpe7-4.fna&oh=00_AT8fKAJfvmDiXqgxxtdfRCIvg6MMKPqP2Jw0dCsuEvFtWw&oe=6308846E",
         description: "Best Ramen Out There.",
         rating: '4'
     },
     {
         name: "Burger King",
-        img: "https://cdn.shopify.com/s/files/v2/upload/",
+        img: "https://cdn.cdnlogo.com/logos/b/73/burger-king.svg",
         description: "King of Burgers.",
         rating: '4'
     }
@@ -42,6 +42,11 @@ restaurantData.forEach((rest) => {
     span.className = 'desc'
     div.appendChild(span)
     body.append(div)
+    const img = document.createElement('img')
+    img.src = rest.img
+    img.alt = rest.name
+    img.className = 'restImg'
+    div.append(img)
     let numbs = rest.rating.split('.')
     for(i=0;i<numbs[0]; i++){
         let div3 = document.createElement('i')
@@ -52,5 +57,5 @@ restaurantData.forEach((rest) => {
         let div4 = document.createElement('i')
         div4.className = 'bi bi-star-half'
         div.append(div4)
-       }
+     }
 })
