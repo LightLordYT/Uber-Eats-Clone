b30fb7991bde804dabdc0dfad09994ee274a9ac5
LightLord <stevensah88@gmail.com>
2022-07-31 11:14:31 -0700

    themes

--- settings.js
+++ settings.js
@@ -1,5 +1,41 @@
-const settings = []
+const settings = [
+    {
+        id: "themes",
+        name: 'Dark Theme',
+        btn: 'theme',
+        btncallback: themeCallback
+    }
+]
 
-const setting = (name, code) => {
-    let div = document.createElement('div')
+function themeCallback(){
+    const btn = document.getElementById('theme')
+    if(btn.innerHTML= 'Off'){
+        btn.innerHTML = 'On'
+        darkTheme()
+    }else{
+        btn.innerHTML = 'Off'
+        lightTheme()
+    }
 }
+
+function darkTheme(){
+
+}
+
+function lightTheme(){
+
+}
+
+settings.forEach((setting) => {
+    let body = document.body
+    let div = document.createElement('div')
+    let btn = document.createElement('button')
+    div.id = setting.id
+    div.className = 'settings'
+    div.innerHTML = setting.name
+    btn.id = setting.btn
+    btn.innerHTML = 'On'
+    btn.addEventListener('click',setting. btncallback)
+    body.append(div)
+    div.appendChild(btn)
+})
